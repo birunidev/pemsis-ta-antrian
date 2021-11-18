a3949238513803a981aa578adb3343b116d4faaf
Muhammad Al Biruni <birunimamak@gmail.com>
2021-11-18 11:10:01 +0700

    initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function next(Request $request){
+        $data = $request->all();
+
+        $data_to_response = [
+            'antrian_aktif' => $data['antrian_aktif'] + 1
+        ];
+        
+        // simpen to db
+        
+
+        
+        return response()->json([
+            'status' => 'oke',
+            'data' => $data_to_response
+        ]);
+    }
+}
